47984cff568daf584e9a02cc7ed90d5bbd589940
Mick Evans <codemastermick@gmail.com>
2020-08-27 10:42:12 -0400

    feat: add flex layout support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import {DemoMaterialModule} from './material-module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { DemoMaterialModule } from './material-module';
 import { DarkModeService } from './services/darkmode.service';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
 import { SidenavService } from './sidenav/sidenav.service';
 import { ProjectCardComponent } from './components/project-card/project-card.component';
 
-
 @NgModule({
   declarations: [
     AppComponent,
     ToolbarComponent,
     SidenavComponent,
-    ProjectCardComponent
+    ProjectCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    DemoMaterialModule
+    DemoMaterialModule,
+    FlexLayoutModule,
   ],
-  providers: [SidenavService,DarkModeService],
-  bootstrap: [AppComponent]
+  providers: [SidenavService, DarkModeService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
