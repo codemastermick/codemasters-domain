ae45942670794ebf6f779559b7e49ac298a46bb6
Mick Evans <codemastermick@gmail.com>
2020-08-29 11:08:41 -0400

    text: fix test breaking from last commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -32,6 +32,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.info').textContent).toContain("Codemaster Mick's Domain is undergoing active development. Your patience is appreciated while we get this done.");
+    expect(compiled.querySelector('.info').textContent).toContain("Codemaster Mick's Domain is undergoing active development. Your patience is appreciated while I get this done.");
   });
 });
