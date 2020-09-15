131484398446588ae9dcdb0d9eb6182fa328e661
Mick Evans <codemastermick@gmail.com>
2020-09-14 23:51:43 -0400

    chore: setup initial home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,13 +3,22 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  greetings = [
+    'Welcome to my corner of the internet',
+    'You found me!',
+    'You were looking for me?',
+    "Codemaster Mick, at your service",
+    "What can I do for you today?",
+  ];
 
-  constructor() { }
+  greeting: string;
+
+  constructor() {}
 
   ngOnInit(): void {
+    this.greeting = this.greetings[~~(Math.random() * this.greetings.length)];
   }
-
 }
